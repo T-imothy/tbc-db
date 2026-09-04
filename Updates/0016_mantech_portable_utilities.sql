a4bb05902b70f6e2e42d29f2d42b4698404682da
Tim <98615444+T-imothy@users.noreply.github.com>
2026-09-03 19:49:04 -0500

    Document ten-minute portable mailbox duration

--- Updates/0016_mantech_portable_utilities.sql
+++ Updates/0016_mantech_portable_utilities.sql
@@ -35,7 +35,7 @@ UPDATE `mantech_item_seed` SET
     `spellcategory_1` = 0,
     `spellcategorycooldown_1` = 0,
     `bonding` = 1,
-    `description` = 'Deploys a mailbox for 5 minutes. Reusable. 10 minute cooldown.',
+    `description` = 'Deploys a mailbox for 10 minutes. Reusable. 10 minute cooldown.',
     `ScriptName` = '',
     `Duration` = 0;
 INSERT INTO `item_template` SELECT * FROM `mantech_item_seed`;
